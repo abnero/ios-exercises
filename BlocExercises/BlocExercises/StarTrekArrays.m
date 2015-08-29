0d6ba2d12553dcf99562f1af75f1b12f6fbcaf92
abnero <abnero@gmail.com>
2015-08-29 16:56:02 -0700

    Assignment 13 completed

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -11,23 +11,43 @@
 @implementation StarTrekArrays
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
-    /* WORK HERE */
-    return @[];
+    
+    return [characterString componentsSeparatedByString:@";"];
+    
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
-    /* WORK HERE */
-    return @"";
+    
+    return [characterArray componentsJoinedByString:@";"];
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
-    /* WORK HERE */
-    return @[];
+    
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES];
+
+    return [characterArray sortedArrayUsingDescriptors:@[sortDescriptor]];
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
-    /* WORK HERE */
+    
+//    NSPredicate *containsWorf = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'Worf'"];
+//    [characterArray filteredArrayUsingPredicate:containsWorf];
+    //return characterArray;
+    
+    //I used the solution above and it worked just for the first part of the last test but failed on the second part of last test. For the solution below I used the wiki solutions.
+    
+    for (NSString *characterName in characterArray){
+        NSString *lowerCaseName = characterName.lowercaseString;
+        NSRange worfRange = [lowerCaseName rangeOfString:@"worf"];
+        
+        if (worfRange.location != NSNotFound){
+            return YES;
+        }
+        
+       
+    }
     return NO;
 }
+
 
 @end
